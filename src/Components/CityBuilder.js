51d868dfd66cd5303a9e5e49c9850dc073669153
Edel <edelmedina322@gmail.com>
2022-08-16 19:27:30 -0300

    City builder - AI / Player interaction / Submit

--- src/Components/CityBuilder.js
+++ src/Components/CityBuilder.js
@@ -1,39 +1,198 @@
 import React, { useState, useEffect } from "react";
 const CityBuilder = () => {
-    const [AIChoices, setAIChoices] = useState([]);
-    const [playerChoices, setPlayerChoices] = useState([]);
-    
-    useEffect(() => {
-        console.log('mounted');
-    });
+  const [AIChoices, setAIChoices] = useState([]);
+  const [playerChoices, setPlayerChoices] = useState([]);
+  let submitBtn = document.querySelector(".submit");
+  let modalA = document.getElementById("modal-a");
+  let modalB = document.getElementById("modal-b");
+  let modalC = document.getElementById("modal-c");
 
-    const AIChooses = () => {
+  useEffect(() => {
+    AIChooses(getRandomInt(3) + 1);
+    AIChooses(getRandomInt(3) + 1);
+    AIChooses(getRandomInt(3) + 1);
+    console.log("first useEffect triggered");
+  }, []);
 
-    };
+  useEffect(() => {
+    checkStatus();
+    console.log("second useEffect triggered");
+    console.log(playerChoices)
+  }, [playerChoices]);
 
-    const displayElement = () => {
-        
-    };
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
-    const checkStatus = () => {
+  const AIChooses = (element) => {
+    setAIChoices((arr) => [...arr, element]);
+  };
 
-    };
+  const playerChooses = (element, choice) => {
+    switch (element) {
+        case "A":
+            let newArray = [...playerChoices];
+            newArray[0] = choice;
+            setPlayerChoices(newArray);
+            break;
+        case "B":
+            let newArray2 = [...playerChoices];
+            newArray2[1] = choice;
+            setPlayerChoices(newArray2);
+            break;
+        case "C":
+            let newArray3 = [...playerChoices];
+            newArray3[2] = choice;
+            setPlayerChoices(newArray3);
+            break;
+    }
+  };
 
-    const destiny = () => {
+  const displayElement = () => {};
 
-    };
+  const checkStatus = () => {
+    if (playerChoices.length === 3) submitBtn.classList.toggle("submit");
+  };
 
-    const displayHints = () => {
+  const destiny = () => {};
 
-    };
-    
-    return (
-        <div id="city">
-            <button>Element A</button>
-            <button>Element B</button>
-            <button>Element C</button>
-        </div>
-    )
+  const displayHints = () => {};
+
+  return (
+    <div id="city">
+      <dialog id="modal-a">
+        <span>
+          <img></img>
+          <button
+            onClick={() => {
+              modalA.close();
+              playerChooses("A", "1");
+            }}
+          >
+            Pick
+          </button>
+        </span>
+        <span>
+          <img></img>
+          <button
+            onClick={() => {
+              modalA.close();
+              playerChooses("A", "2");
+            }}
+          >
+            Pick
+          </button>
+        </span>
+        <span>
+          <img></img>
+          <button
+            onClick={() => {
+              modalA.close();
+              playerChooses("A", "3");
+            }}
+          >
+            Pick
+          </button>
+        </span>
+      </dialog>
+      <dialog id="modal-b">
+        <span>
+          <img></img>
+          <button
+            onClick={() => {
+              modalB.close();
+              playerChooses("B", "1");
+            }}
+          >
+            Pick
+          </button>
+        </span>
+        <span>
+          <img></img>
+          <button
+            onClick={() => {
+              modalB.close();
+              playerChooses("B", "2");
+            }}
+          >
+            Pick
+          </button>
+        </span>
+        <span>
+          <img></img>
+          <button
+            onClick={() => {
+              modalB.close();
+              playerChooses("B", "3");
+            }}
+          >
+            Pick
+          </button>
+        </span>
+      </dialog>
+      <dialog id="modal-c">
+        <span>
+          <img></img>
+          <button
+            onClick={() => {
+              modalC.close();
+              playerChooses("C", "1");
+            }}
+          >
+            Pick
+          </button>
+        </span>
+        <span>
+          <img></img>
+          <button
+            onClick={() => {
+              modalC.close();
+              playerChooses("C", "2");
+            }}
+          >
+            Pick
+          </button>
+        </span>
+        <span>
+          <img></img>
+          <button
+            onClick={() => {
+              modalC.close();
+              playerChooses("C", "3");
+            }}
+          >
+            Pick
+          </button>
+        </span>
+      </dialog>
+
+      <button
+        onClick={() => {
+          modalA.showModal();
+          console.log(AIChoices);
+        }}
+      >
+        Element A
+      </button>
+      <button
+        onClick={() => {
+          modalB.showModal();
+          console.log(AIChoices);
+        }}
+      >
+        Element B
+      </button>
+      <button
+        onClick={() => {
+          modalC.showModal();
+          console.log(AIChoices);
+        }}
+      >
+        Element C
+      </button>
+      <button className="submit">Submit</button>
+    </div>
+  );
 };
 
 export default CityBuilder;
